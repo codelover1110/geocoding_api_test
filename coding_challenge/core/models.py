6688d727da41a12560bb154437c49f8215c086e6
codelover1110 <code.lover1110@gmx.com>
2023-12-12 12:39:22 -0500

    Updated the model

--- coding_challenge/core/models.py
+++ coding_challenge/core/models.py
@@ -4,7 +4,9 @@ import uuid
 # Create your models here.
 class Geocode(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    formatted_address = models.CharField(null=False, max_length=256)
-    lat = models.CharField(null=False, max_length=100)
-    lng = models.CharField(null=False, max_length=100)
+    formatted_address = models.CharField(null=False, max_length=256, db_index=True)
+    lat = models.DecimalField(null=False, max_digits=12, decimal_places=9, db_index=True)
+    lng = models.DecimalField(null=False, max_digits=12, decimal_places=9, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 

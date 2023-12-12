8757af39347212214c411aa0261bdf69f276a5aa
codelover1110 <code.lover1110@gmx.com>
2023-12-12 15:26:19 -0500

    updated the main

--- coding_challenge/core/models.py
+++ coding_challenge/core/models.py
@@ -8,7 +8,7 @@ class Geocode(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     
     # Formatted address, commonly used in queries, and indexed for faster retrieval
-    formatted_address = models.CharField(null=False, max_length=256, db_index=True)
+    address = models.CharField(null=False, max_length=256, db_index=True)
     
     # Latitude and longitude fields, indexed for efficient spatial queries
     lat = models.DecimalField(null=False, max_digits=12, decimal_places=9, db_index=True)
